d54a2c674802be457db6bdb198fa8c3867df63f0
Metalmerge <ermakovd06@gmail.com>
2023-10-03 20:42:42 -0400

    first addition

--- training_data_converter.py
+++ training_data_converter.py
@@ -3,14 +3,14 @@ import requests
 import json
 
 # Define the path to your JSON file
-json_file_path = "path_to_your_json_file.json"
+json_file_path = "/Users/dimaermakov/Downloads/pks70_export.json"
 
 # Check if the JSON file exists
 if not os.path.exists(json_file_path):
     print(f"JSON file not found at {json_file_path}")
 else:
     # Load data from the JSON file
-    with open(json_file_path, 'r') as json_file:
+    with open(json_file_path, "r") as json_file:
         data = json.load(json_file)
 
     # Create folders if they don't exist
@@ -37,7 +37,7 @@ else:
         if response.status_code == 200:
             # Save the image to the appropriate folder
             image_path = os.path.join(folder_path, file_name)
-            with open(image_path, 'wb') as image_file:
+            with open(image_path, "wb") as image_file:
                 image_file.write(response.content)
                 print(f"Downloaded {file_name} to {folder_path}")
         else:
