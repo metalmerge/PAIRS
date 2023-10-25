13865df8432f5fc553c97f45e6a8e6d0b47bca80
Metalmerge <ermakovd06@gmail.com>
2023-10-25 14:38:27 -0400

    working 2 folder image download

---
+++ convert_to_two_folder_images.py
@@ -0,0 +1,45 @@
+import json
+import os
+import requests
+
+json_file_path = "ratings_checked.json"
+
+# Check if the JSON file exists
+if not os.path.exists(json_file_path):
+    print(f"JSON file not found at {json_file_path}")
+else:
+    # Load data from the JSON file
+    with open(json_file_path, "r") as json_file:
+        data = json.load(json_file)
+
+    # Create folders if they don't exist
+    folder_none = "known_Pulsar_Data_JSON_Folder/radio_frequency_interference"
+    folder_other = "known_Pulsar_Data_JSON_Folder/pulsar_candidate"
+    os.makedirs(folder_none, exist_ok=True)
+    os.makedirs(folder_other, exist_ok=True)
+
+    # Iterate through the data and download images
+    for item in data:
+        file_name = item["file"]
+        result = item["checked"]["result"]
+
+        # Determine the folder based on the result
+        if result == "&lt;none&gt;":
+            folder_path = folder_none
+        else:
+            folder_path = folder_other
+
+        # Download the image
+        image_url = item["remoteUrl"]
+        response = requests.get(image_url)
+
+        if response.status_code == 200:
+            # Save the image to the appropriate folder
+            image_path = os.path.join(folder_path, file_name)
+            with open(image_path, "wb") as image_file:
+                image_file.write(response.content)
+                print(f"Downloaded {file_name} to {folder_path}")
+        else:
+            print(f"Failed to download {file_name}")
+
+    print("Download process complete.")
