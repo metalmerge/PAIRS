d2372945f8689d76b10ea9ba63547b050acd9483
Metalmerge <ermakovd06@gmail.com>
2023-10-25 14:49:10 -0400

    fixed the 2 images files

---
+++ convert_to_one_folder_images.py
@@ -0,0 +1,36 @@
+import json
+import os
+import requests
+from PIL import Image
+
+json_file_path = "ratings_Unchecked.json"
+output_folder_path = "Pulsar_Dataset_unchecked"
+
+if not os.path.exists(json_file_path):
+    print(f"JSON file not found at {json_file_path}")
+else:
+    # Load data from the JSON file
+    with open(json_file_path, "r") as json_file:
+        data = json.load(json_file)
+    # Create folders if they don't exist
+    os.makedirs(output_folder_path, exist_ok=True)
+    # Iterate through the data and download images
+    for item in data:
+        file_name = item["file"]
+        # result = item["checked"]["result"]
+        # Download the image
+        image_url = item["image_url"]
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            # Save the image to the appropriate folder
+            image_path = os.path.join(output_folder_path, file_name)
+            with open(image_path, "wb") as image_file:
+                image_file.write(response.content)
+                print(f"Downloaded {file_name} to {output_folder_path}")
+            image = Image.open(image_path)
+            # image = image.resize((1100, 850))
+            image.save(image_path)
+        else:
+            print(f"Failed to download {file_name}")
+
+    print("Download and resize process complete.")
--- unchecked_to_images.py
+++ unchecked_to_images.py
@@ -5,48 +5,10 @@ from PIL import Image
 from roboflow import Roboflow
 
 # Define the path to your JSON file
-json_file_path = "/Users/dimaermakov/Downloads/ratings_pks70a_unchecked.json"
-
-# Check if the JSON file exists
-# if not os.path.exists(json_file_path):
-#     print(f"JSON file not found at {json_file_path}")
-# else:
-#     # Load data from the JSON file
-#     with open(json_file_path, "r") as json_file:
-#         data = json.load(json_file)
-
-#     # Create folders if they don't exist
-#     folder_path = "Pulsar_Dataset"
-#     os.makedirs(folder_path, exist_ok=True)
-
-#     # Iterate through the data and download images
-#     for item in data:
-#         file_name = item["file"]
-#         # result = item["checked"]["result"]
-
-#         # Download the image
-#         image_url = item["image_url"]
-#         response = requests.get(image_url)
-
-#         if response.status_code == 200:
-#             # Save the image to the appropriate folder
-#             image_path = os.path.join(folder_path, file_name)
-#             with open(image_path, "wb") as image_file:
-#                 image_file.write(response.content)
-#                 print(f"Downloaded {file_name} to {folder_path}")
-#             image = Image.open(image_path)
-#             image = image.resize((1100, 850))
-#             image.save(image_path)
-#         else:
-#             print(f"Failed to download {file_name}")
-
-#     print("Download and resize process complete.")
-
-image_folder_path = "/Users/dimaermakov/Downloads/Pulsar_Dataset_unchecked"
-
-# Create the "found_pulsars" folder if it doesn't exist
-output_folder = "/Users/dimaermakov/PULSAR/found_pulsars"
-os.makedirs(output_folder, exist_ok=True)
+json_file_path = "ratings_Unchecked.json"
+image_folder_path = "/Pulsar_Dataset_unchecked"
+output_folder_path = "found_Pulsars_Images_Dataset"
+os.makedirs(output_folder_path, exist_ok=True)
 
 # Initialize Roboflow API
 api_key = os.environ.get("ROBOFLOW_API_KEY")
@@ -71,25 +33,25 @@ def predict_and_save(image_path):
     top_prediction = predictions["predictions"][0]["top"]
     confidence = predictions["predictions"][0]["confidence"]
 
-    if top_prediction == "pulsars" and confidence >= 0.994:
+    if top_prediction == "pulsar_candidate" and confidence >= 0.994:
         image_name = os.path.basename(image_path)
 
-        if not os.path.exists(os.path.join(output_folder, image_name)):
+        if not os.path.exists(os.path.join(output_folder_path, image_name)):
             # Copy the image to the "found_pulsars" folder
             print(f"Found pulsar: {confidence}")
-            output_path = os.path.join(output_folder, image_name)
+            output_path = os.path.join(output_folder_path, image_name)
             os.system(f"cp {image_path} {output_path}")
             print(f"Saved: {image_name}")
             saved_file_count += 1
         else:
             print(f"File with the same name already exists: {image_name}")
-        if saved_file_count >= 100:
-            print("Reached the limit of 100 saved files. Stopping.")
-            # Exit the function to stop processing more files
-            os.system(
-                'osascript -e \'display notification "Predictions Done" with title "Done"\''
-            )
-            exit()
+        # if saved_file_count >= 100:
+        #     print("Reached the limit of 100 saved files. Stopping.")
+        #     # Exit the function to stop processing more files
+        #     os.system(
+        #         'osascript -e \'display notification "Predictions Done" with title "Done"\''
+        #     )
+        #     exit()
 
 
 # Iterate through images in the folder
