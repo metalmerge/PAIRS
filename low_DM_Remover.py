f1fa012e0d4fb0133028010494a8b6c131e0b4ec
Metalmerge <ermakovd06@gmail.com>
2023-11-04 14:57:16 -0400

    Add JSON output file for pulsar candidates with DM > 1

--- low_DM_Remover.py
+++ low_DM_Remover.py
@@ -35,13 +35,13 @@ for item in data:
     # print(image_url)
     if image_url:
         text = extract_text_from_coordinates(image_url, 826, 138, 933, 156)
-        # print(text)
         numeric_value = text_to_float(text)
         if numeric_value is not None and numeric_value > 1:
+            # print(numeric_value)
             filtered_data.append(item)
 
 # Save the filtered data to a new JSON file
-with open("JSON_output/no_Low_DM_Found_Candidates.json", "w") as output_file:
+with open("JSON_output/above_One_found_Pulsar_Candidates.json", "w") as output_file:
     json.dump(filtered_data, output_file, indent=4)
 
 print("Extraction and filtering complete. Check 'output.json' for the filtered data.")
